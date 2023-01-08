47483498ec538f1089f0e16fac5ba7da65df040e
sandip123github <sandipagoturi1606@gmail.com>
2023-01-08 16:47:23 +0530

    message

---
+++ src/com/arraylist/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.arraylist;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		List <String> list = new ArrayList<String>();
+		list.add("sandip");
+		list.add("manish");
+		list.add("subodh");
+		
+		System.out.println(list);
+	}
+
+}
