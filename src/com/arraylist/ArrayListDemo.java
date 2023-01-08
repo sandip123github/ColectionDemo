53d2a99a574c7512106ace259469400a546e84c8
sandip123github <sandipagoturi1606@gmail.com>
2023-01-08 17:05:18 +0530

    message

--- src/com/arraylist/ArrayListDemo.java
+++ src/com/arraylist/ArrayListDemo.java
@@ -15,6 +15,11 @@ public class ArrayListDemo {
 		list.add("subodh");
 		
 		System.out.println(list);
+		System.out.println("subodh");
+		System.out.println("subodh");
+		System.out.println("subodh");
+		System.out.println("subodh");
+		
 	}
 
 }
